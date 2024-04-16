4f380880ab087074152e48fde253c366ca99335c
qkqajrrhtj <qkqajrrhtj@naver.com>
2024-04-16 12:11:32 +0900

    프론트에서 받아온 데이터 처리

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -16,8 +16,19 @@ const login = () => {
     },
     body: JSON.stringify(req),
   })
-    .then((res) => res.json())
-    .then((res) => console.log(res));
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      if (res.success) {
+        location.href = '/';
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error('로그인중 에러발생'));
+    });
 };
 
 loginBtn.addEventListener('click', login);
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -31,7 +31,7 @@ const process = {
         });
       }
     }
-    return res.jsom({
+    return res.json({
       success: false,
       msg: '로그인에 실패하였습니다.',
     });
